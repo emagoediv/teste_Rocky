ea16503cd46ab787298e8b05fb0ab31c59bda3a7
Matheus Amorim <81570051+emagoediv@users.noreply.github.com>
2021-11-04 22:48:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
+// pegando elementos
 const btnMobile = document.getElementById('btn-mobile')
+const btnVoltarTopo = document.getElementById('l-top')
 
-function aparecerMenu(){
-  const nav = document.getElementById('nav') 
-  nav.classList.toggle('active')
-
-}
-
+// escutando evento de clicks
 btnMobile.addEventListener('click', aparecerMenu)
+btnVoltarTopo.addEventListener('click',rolar)
 
+// mudar imagens conforme tamanho da tela a qual o site for carregado
 
-
-setInterval( () =>{
 if(window.screen.width <= 950){
   logo.src = "images/mobile/logo.png"
-  f1.style.backgroundImage = 'url(images/mobile/s3ft1.png)'
+  f1.style.backgroundImage = 'url(images/mobile/s3ft1.png), url(images/web/bg.png)'
   f2.style.backgroundImage = 'url(images/mobile/s3ft2.png)'
   f3.style.backgroundImage = 'url(images/mobile/s3ft3.png)'
   f4.style.backgroundImage = 'url(images/mobile/s4ft1.png)'
@@ -28,4 +25,22 @@ if(window.screen.width <= 950){
   f5.style.backgroundImage = 'url(images/web/s4ft2.png)'
   f6.style.backgroundImage = 'url(images/web/s4ft3.png)'
 }
-},1000)
+
+// funcoes
+
+
+  // efeito de rolagem do btnVoltar
+function rolar(){
+window.scroll({ 
+  top: 0,
+  left: 0,
+  behavior: 'smooth'
+})
+}
+
+
+// adiciona ou remove class que faz aparecer o menu ul no css
+function aparecerMenu(){
+  const nav = document.getElementById('nav') 
+  nav.classList.toggle('active')
+}
